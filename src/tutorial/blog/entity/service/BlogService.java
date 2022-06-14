bcca33dbeeca196a849085c008f056deca825558
KITS_027 <KITS_027@DESKTOP-KS0CH28>
2022-06-14 18:29:50 +0700

    Update

--- src/tutorial/blog/entity/service/BlogService.java
+++ src/tutorial/blog/entity/service/BlogService.java
@@ -1,5 +1,10 @@
 package tutorial.blog.entity.service;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 public class BlogService {
 	private BlogMemorDao  dao ;
 	public BlogService() {
@@ -9,5 +14,121 @@ public class BlogService {
 	public BlogService(BlogMemorDao blog) {
 		super();
 		this.dao =blog ;
+	}
+
+	public Map<String, String> process(String cmd) {
+		// TODO Auto-generated method stub
+		Map<String, String > model = new HashMap< String , String >();
+		if(! cmd.contains("?")) {
+			
+		}else {
+			
+//		has query string , regex : * \ () [] de kiem tra ky hieu phai them 2 dau \\ 
+		String[] req = cmd.split("\\?");
+		System.out.println(req.length);
+		if(req.length==2) {
+				String command = req[0];
+				String parameter = req[1];
+				
+				StringTokenizer tokenizer = new StringTokenizer(parameter,"&");
+             while( tokenizer.hasMoreTokens()) {
+					
+					System.out.println(tokenizer.nextToken());
+					while( tokenizer.hasMoreTokens()) {
+						
+						System.out.println(tokenizer.nextToken("="));
+					}
+				}
+				
+		}else {
+				
+		registPost(model);
+			getPostAll(model);
+			getPostById(model);
+			getPostWithCategory(model);
+			getPostByString(model);
+			updatePost(model);
+			deletePost(model);
+			
+			registCategory(model);
+			getCategoryAll(model);
+			getCategoryById(model);
+			getCategoryWithCategory(model);
+			getCategoryByString(model);
+			updateCategory(model);
+			deleteCategory(model);
+		  }
+		}
+		return model;
+	}
+
+	private void deleteCategory(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void updateCategory(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void getCategoryByString(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void getCategoryById(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void getCategoryWithCategory(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void getCategoryAll(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void registCategory(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void deletePost(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void updatePost(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void getPostByString(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void getPostWithCategory(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void getPostById(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void getPostAll(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void registPost(Map<String, String> model) {
+		// TODO Auto-generated method stub
+		
 	};
 }
--- src/tutorial/blog1/App1.java
+++ src/tutorial/blog1/App1.java
@@ -1,6 +1,8 @@
 package tutorial.blog1;
 
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 import tutorial.blog.entity.service.BlogMemorDao;
 import tutorial.blog.entity.service.BlogService;
@@ -11,6 +13,7 @@ import tutorial.blog.entity.service.BlogService;
  * @email minhduong@gmail.com
  * */
 public class App1 {
+//	 Set iterator
 		public static void main(String[] args) {
 			App1.run(new BlogService(new BlogMemorDao()));
 		}
@@ -21,8 +24,21 @@ public class App1 {
 				do {
 						System.out.println("cmd >");
 						cmd = sc.nextLine();
+						Map<String, String> map = service.process(cmd);
+						 Set<String> keys = map.keySet();
+//						 enhanced for 
+//						tim xem trong Set va Map no khac nhau va giong nhau nhu nao ve method
+//						 Set iterator
+					
+//						 for (String key : keys) {
+//							 System.out.println(key + map.get(key));
+//						 }
+						 
+						 
 				}while(!"exit".equalsIgnoreCase(cmd));
 				System.out.println("terminal");
 				sc.close();
+				
+				
 		}
 }	
